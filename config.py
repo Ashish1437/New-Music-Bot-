9d982f2fdf127ec67f8b86340b612befff488f68
Ashish1437 <121593481+Ashish1437@users.noreply.github.com>
2024-03-18 12:08:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ LOGGER_ID = int(getenv("LOGGER_ID"))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002011084696"))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = getenv("OWNER_USERNAME","imStubborn0")
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
